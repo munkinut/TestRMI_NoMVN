60884e17e9b91e01e43dd8247a21842f3adf26b9
Warren Milburn <warren@munki.net>
2020-05-27 12:37:14 +0100

    Initial commit.

---
+++ copy_serve_files.bat
@@ -0,0 +1,4 @@
+copy .\out\production\TestRMI_NoMVN\net\munki\play\rmi\client\Pi.class .\serve
+copy .\out\production\TestRMI_NoMVN\net\munki\play\rmi\interfaces\Compute.class .\serve
+copy .\out\artifacts\ComputeInterfaces\ComputeInterfaces.jar .\serve
+
